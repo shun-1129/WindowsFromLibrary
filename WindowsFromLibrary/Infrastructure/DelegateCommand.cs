d586fdc9cef605b6e111e88fde05d861aa9b21ab
shun-1129 <shun.1129.y@gmail.com>
2024-06-22 12:06:09 +0900

    コマンド機能、プロパティバインドヘルパー機能追加

---
+++ WindowsFromLibrary/Infrastructure/DelegateCommand.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WindowsFromLibrary.Infrastructure
+{
+    public class DelegateCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private readonly Action _action;
+        private readonly Func<bool> _canExecute;
+
+        public bool CanExecuteProperty
+        {
+            get => CanExecute ();
+        }
+
+        public DelegateCommand ( Action action , Func<bool> canExecute = default )
+        {
+            this._action = action;
+            this._canExecute = canExecute;
+        }
+
+        public bool CanExecute ( object parameter )
+        {
+            return _canExecute?.Invoke () ?? true;
+        }
+
+        public bool CanExecute ()
+        {
+            return _canExecute?.Invoke () ?? true;
+        }
+
+        public void Execute ( object parameter )
+        {
+            _action?.Invoke ();
+        }
+
+        public void Execute ()
+        {
+            _action?.Invoke ();
+        }
+
+        public void DelegateCanExecute ()
+        {
+            CanExecuteChanged?.Invoke ( this , EventArgs.Empty );
+        }
+    }
+
+    public class DelegateCommand<T> : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private readonly Action<T> _action;
+        private readonly Func<T, bool> _canExecute;
+
+        public DelegateCommand ( Action<T> action , Func<T , bool> canExecute = default )
+        {
+            this._action = action;
+            this._canExecute = canExecute;
+        }
+
+        public bool CanExecute ( object parameter )
+        {
+            return _canExecute?.Invoke ( ( T ) parameter ) ?? true;
+        }
+
+        public void Execute ( object parameter )
+        {
+            _action?.Invoke ( ( T ) parameter );
+        }
+
+        public void DelegateCanExecute ()
+        {
+            CanExecuteChanged?.Invoke ( this , EventArgs.Empty );
+        }
+    }
+}
---
+++ WindowsFromLibrary/Infrastructure/PropertyBindHelper.cs
@@ -0,0 +1,128 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace WindowsFromLibrary.Infrastructure
+{
+    public static class PropertyBindHelper
+    {
+        /// <summary>
+        /// 値、状態をバインド
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="item1">バインドするプロパティの Expression</param>
+        /// <param name="item2">バインドする値のExpression</param>
+        /// <exception cref="ArgumentException"></exception>
+        public static void Bind<T, U> ( Expression<Func<T>> item1 , Expression<Func<U>> item2 )
+        {
+            Tuple<object , string> ResolveLambda<V> ( Expression<Func<V>> expression )
+            {
+                var lambda = expression as LambdaExpression;
+                if ( lambda == null ) throw new ArgumentException ();
+                var property = lambda.Body as MemberExpression;
+                if ( property == null ) throw new ArgumentException ();
+                var members = new List<MemberInfo>();
+                var parent = property.Expression;
+                return new Tuple<object , string> ( Expression.Lambda ( parent ).Compile ().DynamicInvoke () , property.Member.Name );
+            }
+            var tuple1 = ResolveLambda(item1);
+            var tuple2 = ResolveLambda(item2);
+            var control = tuple1.Item1 as Control;
+            if ( control == null ) throw new ArgumentException ();
+            control.DataBindings.Add ( new Binding ( tuple1.Item2 , tuple2.Item1 , tuple2.Item2 ) );
+        }
+
+        /// <summary>
+        /// TextBoxへ値をバインド
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="textBox">バインドするプロパティ</param>
+        /// <param name="expression">バインドする値</param>
+        public static void Bind<T> ( TextBox textBox , Expression<Func<T>> expression )
+        {
+            Bind ( () => textBox.Text , expression );
+        }
+
+        /// <summary>
+        /// Labelへ値をバインド
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="label">バインドするプロパティ</param>
+        /// <param name="expression">バインドする値</param>
+        public static void Bind<T> ( Label label , Expression<Func<T>> expression )
+        {
+            Bind ( () => label.Text , expression );
+        }
+
+        /// <summary>
+        /// DataGridView
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dataGridView">バインドするプロパティ</param>
+        /// <param name="expression">バインドする値</param>
+        public static void Bind<T> ( DataGridView dataGridView , Expression<Func<T>> expression )
+        {
+            Bind ( () => dataGridView.DataSource , expression );
+        }
+
+        /// <summary>
+        /// Buttonへ値をバインド
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="button">バインドするプロパティ</param>
+        /// <param name="expression">バインドする値</param>
+        public static void Bind<T> ( Button button , Expression<Func<T>> expression )
+        {
+            Bind ( () => button.Text , expression );
+        }
+
+
+        /// <summary>
+        /// Buttonへ値をバインド
+        /// </summary>
+        /// <param name="button">バインドするプロパティ</param>
+        /// <param name="expression">バインドする値</param>
+        public static void Bind ( Button button , DelegateCommand command )
+        {
+            button.Click += ( sender , aegs ) => { command.Execute (); };
+        }
+
+        /// <summary>
+        /// Buttonへ値をバインド
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="button">バインドするプロパティ</param>
+        /// <param name="expression">バインドする値</param>
+        public static void Bind<T> (Button button , DelegateCommand<T> command )
+        {
+            button.Click += ( sender , aegs ) => { command.Execute ( aegs ); };
+        }
+
+        /// <summary>
+        /// Buttonへ値をバインドする
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="button">バインドするプロパティ</param>
+        /// <param name="command">バインドする値</param>
+        /// <param name="expression">バインドする値</param>
+        public static void Bind<T> ( Button button , DelegateCommand command , Expression<Func<T>> expression )
+        {
+            Bind ( () => button.Text , expression );
+            button.Click += ( sender , aegs ) => { command.Execute (); };
+        }
+
+        /// <summary>
+        /// Buttonへ値をバインドする
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="button">バインドするプロパティ</param>
+        /// <param name="command">バインドする値</param>
+        /// <param name="expression">バインドする値</param>
+        public static void Bind<T, U> ( Button button , DelegateCommand<T> command , Expression<Func<U>> expression )
+        {
+            Bind ( () => button.Text , expression );
+            button.Click += ( sender , aegs ) => { command.Execute (aegs); };
+        }
+    }
+}
