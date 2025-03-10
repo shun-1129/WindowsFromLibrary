86e2a04a59bfd6f3814865b9e8b2d35b64ea3b5e
shun-1129 <shun.1129.y@gmail.com>
2025-03-10 23:49:41 +0900

    [Add] 長方形トグルボタンクラス追加（処理未定義）

---
+++ WindowsFromLibrary/CustomControls/BaseToggleButton.cs
@@ -0,0 +1,19 @@
+﻿namespace WindowsFromLibrary.CustomControls
+{
+    /// <summary>
+    /// トグルボタン基底クラス
+    /// </summary>
+    public abstract class BaseToggleButton : CheckBox
+    {
+        #region プロパティ
+        /// <summary>
+        /// ON時背景色
+        /// </summary>
+        public abstract Color OnBackColor { get; set; }
+        /// <summary>
+        /// OFF時背景色
+        /// </summary>
+        public abstract Color OffBackColor { get; set; }
+        #endregion
+    }
+}
---
+++ WindowsFromLibrary/CustomControls/RectangleToggleButton.cs
@@ -0,0 +1,42 @@
+﻿using WindowsFromLibrary.CustomControls.Models;
+
+namespace WindowsFromLibrary.CustomControls
+{
+    /// <summary>
+    /// 長方形トグルボタン
+    /// </summary>
+    public class RectangleToggleButton : BaseToggleButton
+    {
+        #region メンバ変数
+        private Color _onBackColor = ToggleButtonDefinition.DEFAULT_ON_BACK_COLOR;
+        private Color _offBackColor = ToggleButtonDefinition.DEFAULT_OFF_BACK_COLOR;
+        #endregion
+
+        #region プロパティ
+        /// <summary>
+        /// 
+        /// </summary>
+        public override Color OnBackColor
+        {
+            get => _onBackColor;
+            set
+            {
+                _onBackColor = value;
+                this.Invalidate ();
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public override Color OffBackColor
+        {
+            get => _offBackColor;
+            set
+            {
+                _offBackColor = value;
+                this.Invalidate ();
+            }
+        }
+        #endregion
+    }
+}
